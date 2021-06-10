ffbec5537e335294d0ed4962c808bdeb25061ff2
IsmMah <ismat.mahmood@softwire.com>
2021-06-10 17:25:51 +0100

    javascript code added

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript here.
+    if (document.getElementById("random-color")) {
+        const colorClick = document.getElementById("random-color");
+        colorClick.addEventListener("click", randomColor);
+    }
+    
+    const menu = document.getElementById("nav-button");
+    menu.addEventListener("click", expandMenu);
+    //document.
+    document.getElementById("main").addEventListener("click", closeMenu);
 });
+
+function randomColor() {
+    const myElement = document.getElementsByClassName("list-items");
+    for (let i=0; i < myElement.length; i++) {
+        myElement[i].style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    }
+}
+
+function expandMenu(){
+    document.getElementById("nav-bar").classList.toggle('show');
+}
+
+function closeMenu() {
+    if (document.getElementsByClassName("show").length > 0) {
+        document.getElementById("nav-bar").classList.toggle('show');
+    }
+}
+
